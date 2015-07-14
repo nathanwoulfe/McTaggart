51b8a42e1978acaa12dc6a588ec119e5c0809865
Nathan Woulfe <nathan@nathanw.com.au>
2015-07-14 16:02:38 +1000

    Initial commit

---
+++ App_Plugins/TagYoureIt/tagyoureit.controller.js
@@ -0,0 +1,69 @@
+﻿angular.module('umbraco').controller('tagyoureit.controller', ['$scope', 'editorState', 'contentResource', 'contentEditingHelper', 'tagFactory', function ($scope, editorState, contentResource, contentEditingHelper, tagFactory) {
+
+    $scope.msg = 'tag';
+
+    var apiKey = $scope.model.config.apiKey,
+        alias = $scope.model.config.properties.split(',');
+        currentId = editorState.getCurrent().id;
+
+    $scope.tag = function () {
+
+        contentResource.getById(currentId).then(function (resp) {
+
+            var props = contentEditingHelper.getAllProps(resp),
+                data;
+
+            angular.forEach(alias, function (a) {
+                data += $.grep(props, function (p) {
+                    return p.alias === a;
+                })[0].value;
+            })
+
+            tagFactory.getTags(data, apiKey).then(function (resp) {
+                parseTags(resp);
+            });
+        });
+    }
+
+    var parseTags = function (d) {
+        $scope.tags = [];
+
+        angular.forEach(d, function (o) {
+            if (o._typeGroup === 'socialTag') {
+                $scope.tags.push(o.name);
+            }
+        });
+
+        $scope.model.value = $scope.tags !== $scope.model.value ? $scope.tags : $scope.model.value;
+        $scope.$apply();
+    }
+
+    $scope.removeTag = function (t) {
+        var i = $scope.model.value.indexOf(t);
+        $scope.model.value.splice(i, 1);
+    }
+
+}]);
+
+angular.module('umbraco.resources').factory('tagFactory', function ($http) {
+    return {
+        getTags: function (d, apiKey) {
+
+            var ajaxOpts = {
+                type: 'POST',
+                data: d,
+                url: "https://api.thomsonreuters.com/permid/calais",
+                beforeSend: function (jqXHR, settings) {
+                    jqXHR.setRequestHeader('Content-Type', 'text/raw');
+                    jqXHR.setRequestHeader('OutputFormat', 'application/json');
+                    jqXHR.setRequestHeader('X-AG-Access-Token', apiKey);
+                },
+                success: function (data, textStatus, jqXHR) {
+                    return data;
+                }
+            }
+
+            return $.ajax(ajaxOpts);
+        }
+    }
+});
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # McTaggart
-Auto-tagging for Umbraco
+## Auto-tagging for Umbraco
+
+
+
+John McTaggart Ellis McTaggart (yes, double McTaggart) was an idealist metaphysician, concerned with the fundamental nature of being and the world that encompasses it.
+
+This McTaggart is only concerned with understanding Umbraco content.
+
+McTaggart uses the Open Calais API to generate tags from HTML strings. Don't think about tags for your content, just hit the button and let McTaggart do the hard work.
+
+Allowing content creators to add their own tags or keywords is fine, except you'll end up with a tag set representing what the author thinks they've written about - McTaggart, by way of Open Calias, analyses the text to return tags that relate directly to the content and can thne be used to generate more accurate search result sets.
+
+McTaggart is a simple property editor - add it to an Umbraco project, create a datatype using the McTaggart editor, add your API key and the aliases of the properties to analyse, and you're good to go.
