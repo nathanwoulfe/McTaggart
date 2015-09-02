66649f48c6d78ddc5bb493c768243048bad5da94
Nathan Woulfe <nathan@nathanw.com.au>
2015-09-02 11:29:19 +1000

    .

--- App_Plugins/McTaggart/mctaggart.controller.js
+++ App_Plugins/McTaggart/mctaggart.controller.js
@@ -1,32 +1,202 @@
-﻿angular.module('umbraco').controller('mctaggart.controller', ['$scope', 'editorState', 'tagFactory', function ($scope, editorState, tagFactory) {
+﻿/* Extends the core Umbraco tags property editor to include auto-generated tags from the OpenCalais API */
+/* API key -> jz5VUywxNNq4Lur3S7ie9gslFsvoIcHl */
 
-    $scope.tag = function () {
+angular.module('umbraco').controller('mctaggart.controller', ['$rootScope', '$scope', 'assetsService', 'editorState', 'tagFactory', '$element', 'angularHelper', 'umbRequestHelper', '$timeout',
+    function ($rootScope, $scope, assetsService, editorState, tagFactory, $element, angularHelper, umbRequestHelper, $timeout) {
 
-        $scope.model.value = [];
-        $scope.loading = true;
+    var $typeahed;
+    $scope.tagToAdd = ''; 
 
-        tagFactory.getTags($scope.model.config.apiKey, editorState.getCurrent().id, $scope.model.config.properties).then(function (resp) {
-
-            angular.forEach(resp.data, function (o) {
-                $scope.model.value.push(o);
-            });
-
-            $scope.loading = false;
-        });
+    // Method required by the valPropertyValidator directive (returns true if the property editor has at least one tag selected)
+    $scope.validateMandatory = function () {
+        return {
+            isValid: !$scope.model.validation.mandatory || ($scope.model.value != null && $scope.model.value.length > 0),
+            errorMsg: "Value cannot be empty",
+            errorKey: "required"
+        };
     }
 
-    $scope.removeTag = function (t) {
-        $scope.model.value.splice($scope.model.value.indexOf(t), 1);
-    }
-
+    // Set the default text - either loading tags or none exist
     $scope.placeholder = function () {
         return $scope.loading ? 'Thinking...' : 'There\'s nothing here, yet';
     }
 
+    // Do we display the placeholder text? Only if the model value is empty
     $scope.showPlaceholder = function () {
         return $scope.model.value.length === 0;
     }
 
+    function checkStorageType() {
+        console.log($scope.model.value, $scope.model.config.storageType);
+        if (!$scope.model.config.storageType || $scope.model.config.storageType !== "json") {
+            //it is csv
+            if (!$scope.model.value) {
+                $scope.model.value = [];
+            }
+        }
+    }
+
+    // Fetch the typeahead script and start the fun...
+    assetsService.loadJs("~/App_Plugins/McTaggart/lib/typeahead.bundle.min.js").then(function () {
+
+        // load current value
+        if ($scope.model.value) {
+            checkStorageType();
+        }
+        else {
+            $scope.model.value = [];
+        }
+
+        // Click... polls the OpenCalais API for tag data
+        $scope.tag = function () {
+
+            $scope.loading = true;
+            tagFactory.getTags($scope.model.config.apiKey, editorState.getCurrent().id, $scope.model.config.properties).then(function (resp) {
+
+                // Push the tags into the model value
+                angular.forEach(resp.data, function (o) {
+                    addTag(o);
+                });
+
+                $scope.loading = false;
+            });
+        }
+
+        // Maybe we don't want to keep all suggested tags
+        $scope.removeTag = function (t) {
+            $scope.model.value.splice($scope.model.value.indexOf(t), 1);
+            $scope.propertyForm.tagCount.$setViewValue($scope.model.value.length);
+        }
+
+        // Helper method to add a tag on enter or on typeahead select
+        function addTag(tagToAdd) {
+            if (tagToAdd != null && tagToAdd.length > 0) {
+                if ($scope.model.value.indexOf(tagToAdd) < 0) {
+                    $scope.model.value.push(tagToAdd);
+                    $scope.propertyForm.tagCount.$setViewValue($scope.model.value.length);
+                }
+            }
+        }
+
+        $scope.addTagOnEnter = function (e) {
+            var code = e.keyCode || e.which;
+            if (code == 13) { //Enter keycode   
+                if ($element.find('.tags-' + $scope.model.alias).parent().find(".tt-dropdown-menu .tt-cursor").length === 0) {
+                    //this is required, otherwise the html form will attempt to submit.
+                    e.preventDefault();
+                    $scope.addTag();
+                }
+            }
+        };
+
+        $scope.addTag = function () {
+            //ensure that we're not pressing the enter key whilst selecting a typeahead value from the drop down
+            //we need to use jquery because typeahead duplicates the text box
+            addTag($scope.tagToAdd);
+            $scope.tagToAdd = "";
+            //this clears the value stored in typeahead so it doesn't try to add the text again
+            // http://issues.umbraco.org/issue/U4-4947
+            $typeahead.typeahead('val', '');
+        };
+
+        //vice versa
+        $scope.model.onValueChanged = function (newVal, oldVal) {
+            //update the display val again if it has changed from the server
+            $scope.model.value = newVal;
+            checkStorageType();
+        };
+
+        //configure the tags data source
+
+        //helper method to format the data for bloodhound
+        function dataTransform(list) {
+            //transform the result to what bloodhound wants
+            var tagList = _.map(list, function (i) {
+                return { value: i.text };
+            });
+
+            // remove current tags from the list
+            return $.grep(tagList, function (tag) {
+                return ($.inArray(tag.value, $scope.model.value) === -1);
+            });
+        }
+
+        // helper method to remove current tags
+        function removeCurrentTagsFromSuggestions(suggestions) {
+            return $.grep(suggestions, function (suggestion) {
+                return ($.inArray(suggestion.value, $scope.model.value) === -1);
+            });
+        }
+
+        var tagsHound = new Bloodhound({
+            datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
+            queryTokenizer: Bloodhound.tokenizers.whitespace,
+            dupDetector: function (remoteMatch, localMatch) {
+                return (remoteMatch["value"] == localMatch["value"]);
+            },
+            //pre-fetch the tags for this category
+            prefetch: {
+                url: umbRequestHelper.getApiUrl("tagsDataBaseUrl", "GetTags", [{ tagGroup: $scope.model.config.group }]),
+                //TTL = 5 minutes
+                ttl: 300000,
+                filter: dataTransform
+            },
+            //dynamically get the tags for this category (they may have changed on the server)
+            remote: {
+                url: umbRequestHelper.getApiUrl("tagsDataBaseUrl", "GetTags", [{ tagGroup: $scope.model.config.group }]),
+                filter: dataTransform
+            }
+        });
+
+        tagsHound.initialize(true);
+
+        //configure the type ahead
+        $timeout(function () {
+
+            $typeahead = $element.find('.tags-' + $scope.model.alias).typeahead(
+            {
+                //This causes some strangeness as it duplicates the textbox, best leave off for now.
+                hint: false,
+                highlight: true,
+                cacheKey: new Date(),  // Force a cache refresh each time the control is initialized
+                minLength: 1
+            }, {
+                //see: https://github.com/twitter/typeahead.js/blob/master/doc/jquery_typeahead.md#options
+                // name = the data set name, we'll make this the tag group name
+                name: $scope.model.config.group,
+                displayKey: "value",
+                source: function (query, cb) {
+                    tagsHound.get(query, function (suggestions) {
+                        console.log(query, suggestions);
+                        cb(removeCurrentTagsFromSuggestions(suggestions));
+                    });
+                },
+            }).bind("typeahead:selected", function (obj, datum, name) {
+                angularHelper.safeApply($scope, function () {
+                    addTag(datum["value"]);
+                    $scope.tagToAdd = "";
+                    // clear the typed text
+                    $typeahead.typeahead('val', '');
+                });
+
+            }).bind("typeahead:autocompleted", function (obj, datum, name) {
+                angularHelper.safeApply($scope, function () {
+                    addTag(datum["value"]);
+                    $scope.tagToAdd = "";
+                });
+
+            }).bind("typeahead:opened", function (obj) {
+                //console.log("opened ");
+            });
+        });
+
+        $scope.$on('$destroy', function () {
+            tagsHound.clearPrefetchCache();
+            tagsHound.clearRemoteCache();
+            $element.find('.tags-' + $scope.model.alias).typeahead('destroy');
+            delete tagsHound;
+        });
+    });
 }]);
 
 angular.module('umbraco.resources').factory('tagFactory', function ($http) {
